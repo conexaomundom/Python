9d0ea123770f99e9f5dca59d524a814c0987718c
Marina Rodrigues de Oliveira <48895083+conexaomundom@users.noreply.github.com>
2022-06-26 13:05:03 -0300

    Simple math operations using Python

---
+++ numeros.py
@@ -0,0 +1,28 @@
+"""Integer"""
+print(2 + 3) # sum
+print(3 - 2) # subtraction,   
+print(2 * 3) # multiplication
+print(3 / 2) # division
+
+print(3 ** 3)
+print(10 ** 6)
+
+# python respects the orders of operations
+print(2 + 3*4) 
+# and also respects order of the brackets
+print((2 + 3)*4) 
+
+"""Floating point"""
+print(2.43 + 3.57) # sum
+print(3.2 - 2.2) # subtraction
+print(2.2 * 3.1) # multiplication
+print(3.3 / 2.3) # division
+
+print(3.43 ** 3.43)
+print(10.1 ** 6.0)
+
+# str(var) converts the value to string
+age = 24
+print('Happy ' + str(age) + 'rd Birthday!!')
+
+
