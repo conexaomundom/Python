60aaf655efb8af8d787c5b435c6e204174432e51
Marina Rodrigues de Oliveira <48895083+conexaomundom@users.noreply.github.com>
2022-06-26 20:59:17 -0300

    A list is an ordered collection, as an array
    ''' One of the most fundamental data structures of python is list
    A list is an ordered collection, as an array, but lists have 
    other features '''

--- lists.py
+++ lists.py
@@ -12,8 +12,29 @@ bicycles = ['treck','cannodale','redline','specialized']
 but not for the ful list because results an error:
 'list' object has no attribute 'title'
 """
+"""
 print(bicycles)
 print(bicycles[0].title()) #first element os th list with fisrt letter upper
 print(bicycles[1]) # second element
 print(bicycles[3]) # fourth element (last element of the list)
-print(bicycles[-1]) # last element of the list (fourth element)
+print(bicycles[-1]) # last element of the list (fourth element)"""
+
+# using indidual values from a list 
+mesage = 'My fisrt bicycle was a ' + bicycles[0].title() + '.'
+
+#print(mesage)
+
+''' One of the most fundamental data structures of python is list
+A list is an ordered collection, as an array, but lists have 
+other features '''
+
+integer_list = [2,6,3]
+heterogeneous_list = ['string', 0.1, True,False]
+# [] is an empty list
+# It is possible to have lists as elements of a list (lists within lists)
+list_of_list = [integer_list, heterogeneous_list, []] 
+list_length = len(heterogeneous_list)
+list_sum = sum(integer_list)
+
+# extending the values of a list (adding)
+integer_list.extend([1,2,3,4,5,6,7,8,9])
