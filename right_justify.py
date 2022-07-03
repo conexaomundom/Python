9a0f5966f14e72b3ec0ab47233b6dff93cfa1635
Marina Rodrigues de Oliveira <48895083+conexaomundom@users.noreply.github.com>
2022-07-03 20:35:42 -0300

    do_twice, do_four
    ''' Exemples using unctions in python
        Book Think Python
    '''

--- right_justify.py
+++ right_justify.py
@@ -1,19 +1,41 @@
-''' functions in python'''
+''' Exemples using unctions in python
+    Book Think Python
+'''
 
-def right_justify(word, n):
-    m = n - len(word)
-    message = ' ' * m + word
+def right_justify(word, qtt):
+    '''puts a qtt of blank space until the word
+     '''
+    message = ' ' * (qtt - len(word)) + word
 
     print(message)
 
-#right_justify('morty', 70)
+# right_justify('morty', 70)
 
-def do_twice(n, f):
-    for i in range(n):
-        f()
+def do_twice(func, argument):
+    ''' Puts the function to run twice
+    
+    func is the object that needs to be a funcion
+    argument is the function argument     '''
+    func(argument)
+    func(argument)
 
 
 def print_spam():
     print('spam')
 
-do_twice(2,print_spam)
+#do_twice(2,print_spam)
+
+def print_twice(name):
+    print(name)
+    print(name)
+    
+# print_twice('spam')
+# do_twice(print_twice, 'spam')
+
+def do_four(func, argument):
+    ''' Puts the function to run four times '''
+    do_twice(func, argument)
+    do_twice(func, argument)
+
+
+do_four(print,'spam')
