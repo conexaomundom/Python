28e50698daf1f2cd2df66a381607e36c373644e0
Marina Rodrigues de Oliveira <48895083+conexaomundom@users.noreply.github.com>
2024-12-18 18:06:13 -0500

    Now the Data Pipeline class call Data class and it has a draft of the other methods

--- DataPipeline.py
+++ DataPipeline.py
@@ -1,49 +1,36 @@
 import pandas as pd
-from faker import Faker
+#from faker import Faker
 import random
+from DataSet import Data
 
 class DataPipeline:
     
-    def __init__(self,ds_size):
+    def __init__(self):
         #Initiazlizate an empty Data frame to enter to be feed with customer data
-        self.ds_size = ds_size
-        self.fake = Faker()
-        self.database = []
         self.analysis_results = None
         self.model_results = None
+        self.database = None
+        self.ds_size = 10000
+        self.data = None
         
-    def generate_records(self, record_id):
-        record = {
-            "ID": record_id,
-            "age": random.randint(18,70),
-            "city": self.fake.city(),
-            "salary": round(random.uniform(20000,120000), 2),
-            "gender":random.choice(["Male","Female"]),
-            "online_shopping": random.choice([True,False])
-        }  
-        
-        return record
-
-    def generate_dataset(self):
-        for i in range(1, self.ds_size + 1):
-            self.database.append(self.generate_records(i))
-            
-    def get_dataset(self):
-        print("Data generated successfully!")
-        return self.database
+    def dataset(self):
+        self.data = Data(self.ds_size)
+        random.seed(10)
+        self.data.generate_dataset()
+        self.database = self.data.get_dataset()
 
     def exploratory_analysis(self):
-        if self.database == []:
+
+        if self.database == None:
             raise ValueError("data has not been generated yet.")
         data = pd.DataFrame(self.database)
         self.analysis_results = data.describe(include = 'all')
         print("Exploratory Analysis completed.")
         
     def model_run(self):
-        if self.database == []:
+        if self.database == None:
             raise ValueError("data has not been generated yet.")
         #self.model_results = "Not done yet." #self.database
-        
         pass
     
     def generate_report(self):
@@ -60,10 +47,9 @@ class DataPipeline:
     
     # Main function to execute the pipeline
 def main():
-    ds_size = 10000
-    pipeline = DataPipeline(ds_size)
-    random.seed(10)
-    pipeline.generate_dataset()
+
+    pipeline = DataPipeline()    
+    pipeline.dataset()
     pipeline.exploratory_analysis()
     pipeline.model_run()
     pipeline.generate_report()
